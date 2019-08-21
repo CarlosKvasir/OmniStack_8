c1c8e3ccc1e9faaedf5422b7729bc4378b8a1eda
Carlos Lima (Kvasir) <carloslimajr.eng@gmail.com>
2019-08-21 12:09:03 -0300

    create post/dev endpoint

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,4 +6,10 @@ routes.get('/', (req, res) => {
   return res.json({ message: `Hello ${req.query.name}` });
 });
 
+routes.post('/dev', (req, res) => {
+  return res.json({
+    data: true,
+  });
+})
+
 module.exports = routes;
