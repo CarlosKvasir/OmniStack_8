8b0e2c33afcc78882256ed6cd7a9bdfc41338c02
Carlos Lima (Kvasir) <carloslimajr.eng@gmail.com>
2019-08-21 23:55:24 -0300

    remove test endpoint

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,10 +3,6 @@ const DevController = require('./controllers/DevController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: `Hello ${req.query.name}` });
-});
-
 routes.post('/devs', DevController.store);
 
 module.exports = routes;
