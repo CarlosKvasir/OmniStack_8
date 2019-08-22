1ad5d892cbcfae259fdb4aa75635cd5b041c4e94
Carlos Lima (Kvasir) <carloslimajr.eng@gmail.com>
2019-08-21 23:26:00 -0300

    mongo + mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv/config'); // use config in .env
+
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect(
+  `mongodb+srv://oministack:${process.env.MONGO_PW}@clustertest-qzssj.mongodb.net/oministack_8?retryWrites=true&w=majority`, {
+    useNewUrlParser: true
+  });
 
 server.use(express.json());
 server.use(routes);
