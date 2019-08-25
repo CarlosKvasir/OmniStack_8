8489cedeaeb4fb8b2bd7c15fc4d26c0bc1d51d97
Carlos Lima (Kvasir) <carloslimajr.eng@gmail.com>
2019-08-25 16:25:57 -0300

    remove some examples html

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -33,54 +33,6 @@ export default function Main({ match }) {
             alt="imagem do xxx" />
           <footer>
             <strong>Marco Bruno</strong>
-            <p>aaaaaaaaa aaaaaaaaaaaa aaaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaaaa Iniciei minha carreira como Dev. de HMI.Apaixonado pela Web, migrei para Dev.Front-End/UX e agora trabalhando na CollabCollab com Front-End e UX</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="https://avatars1.githubusercontent.com/u/4096456?v=4"
-            alt="imagem do xxx" />
-          <footer>
-            <strong>Marco Bruno</strong>
-            <p>Iniciei minha carreira como Dev. de HMI.Apaixonado pela Web, migrei para Dev.Front-End/UX e agora trabalhando na CollabCollab com Front-End e UX</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="https://avatars1.githubusercontent.com/u/4096456?v=4"
-            alt="imagem do xxx" />
-          <footer>
-            <strong>Marco Bruno</strong>
-            <p>Iniciei minha carreira como Dev. de HMI.Apaixonado pela Web, migrei para Dev.Front-End/UX e agora trabalhando na CollabCollab com Front-End e UX</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="https://avatars1.githubusercontent.com/u/4096456?v=4"
-            alt="imagem do xxx" />
-          <footer>
-            <strong>Marco Bruno</strong>
             <p>Iniciei minha carreira como Dev. de HMI.Apaixonado pela Web, migrei para Dev.Front-End/UX e agora trabalhando na CollabCollab com Front-End e UX</p>
           </footer>
           <div className="buttons">
