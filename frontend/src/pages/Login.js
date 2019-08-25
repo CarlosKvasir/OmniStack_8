d58c540c632611deee52efc931f9b7aa98ed826a
Carlos Lima (Kvasir) <carloslimajr.eng@gmail.com>
2019-08-25 12:38:57 -0300

    add form in Login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -6,7 +6,12 @@ import logo from '../assets/logo.svg';
 export default function Login() {
   return (
     <div className="login-container">
-      <img src={logo} alt='Tindev' />
+      <form>
+        <img src={logo} alt='Tindev' />
+        <input
+          placeholder="Digite seu usuário no Github"
+        />
+      </form>
     </div>
   );
 }
