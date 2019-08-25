f87a821d9539a45e526eea8e4ac2bc9ab36367a7
Carlos Lima (Kvasir) <carloslimajr.eng@gmail.com>
2019-08-25 13:18:44 -0300

    create routes.js

---
+++ frontend/src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Main from './pages/Main';
+
+export default Routers() {
+  return (
+    <Route />
+  )
+}
