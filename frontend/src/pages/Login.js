7f9f4e066c165573c6e7a877f853e68d212d2e4d
Carlos Lima (Kvasir) <carloslimajr.eng@gmail.com>
2019-08-25 14:44:13 -0300

    using api in Login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 import './Login.css';
 
+import api from '../services/api';
+
 import logo from '../assets/logo.svg';
 
 export default function Login({ history }) {
   const [username, setUsername] = useState('');
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
 
-    console.log(username);
+    const response = await api.post('/devs', {
+      username,
+    })
+    console.log(response);
+
     history.push('/main');
   }
 
