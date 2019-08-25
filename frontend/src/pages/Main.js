8c143895e2dc025b09f444d0b8fd5d586d15e15e
Carlos Lima (Kvasir) <carloslimajr.eng@gmail.com>
2019-08-25 15:12:39 -0300

    create html

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,79 @@
 import React from 'react';
 
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
 export default function Main({ match }) {
-  return <h1>{match.params.id}</h1>
+  return (
+    <div className="main-container">
+      <img src={logo} alt='Tindev' />
+      <ul>
+        <li>
+          <img src="https://avatars1.githubusercontent.com/u/4096456?v=4"
+            alt="imagem do xxx" />
+          <footer>
+            <strong>Marco Bruno</strong>
+            <p>Iniciei minha carreira como Dev. de HMI.Apaixonado pela Web, migrei para Dev.Front-End/UX e agora trabalhando na CollabCollab com Front-End e UX</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars1.githubusercontent.com/u/4096456?v=4"
+            alt="imagem do xxx" />
+          <footer>
+            <strong>Marco Bruno</strong>
+            <p>Iniciei minha carreira como Dev. de HMI.Apaixonado pela Web, migrei para Dev.Front-End/UX e agora trabalhando na CollabCollab com Front-End e UX</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars1.githubusercontent.com/u/4096456?v=4"
+            alt="imagem do xxx" />
+          <footer>
+            <strong>Marco Bruno</strong>
+            <p>Iniciei minha carreira como Dev. de HMI.Apaixonado pela Web, migrei para Dev.Front-End/UX e agora trabalhando na CollabCollab com Front-End e UX</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars1.githubusercontent.com/u/4096456?v=4"
+            alt="imagem do xxx" />
+          <footer>
+            <strong>Marco Bruno</strong>
+            <p>Iniciei minha carreira como Dev. de HMI.Apaixonado pela Web, migrei para Dev.Front-End/UX e agora trabalhando na CollabCollab com Front-End e UX</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
