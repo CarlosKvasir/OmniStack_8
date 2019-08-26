364fb0f48d8eb0ec6557f683fa60555665069dd0
Carlos Lima (Kvasir) <carloslimajr.eng@gmail.com>
2019-08-26 15:28:23 -0300

    using StyleSheet

--- tindev/App.js
+++ tindev/App.js
@@ -7,16 +7,25 @@
  */
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#7159C1',
-      }}>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159C1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 20,
+  },
+});
