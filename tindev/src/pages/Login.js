97902f923dd907f37c777fff5e3f8dbab7012a30
Carlos Lima (Kvasir) <carloslimajr.eng@gmail.com>
2019-08-26 16:23:04 -0300

    centralizando container

--- tindev/src/pages/Login.js
+++ tindev/src/pages/Login.js
@@ -12,5 +12,11 @@ export default function Login() {
 }
 
 const style = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
 });
